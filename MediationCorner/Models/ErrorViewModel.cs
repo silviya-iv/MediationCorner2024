43115eabafc5dce00bab0a0db176af34909e5399
Silviya <Silviya@SILVIYA-PC>
2024-03-30 14:57:31 +0200

    Add project files.

---
+++ MediationCorner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediationCorner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
