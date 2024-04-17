44449c01ebbe4428c970a5264204e4ba3bfbd49d
ivanova.silviya@gmail.com <ivanova.silviya@gmail.com>
2024-04-17 23:07:43 +0300

    AppointmentConfiguration added

---
+++ MediationCorner.Infrastructure/Data/Configurations/AppointmentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MediationCorner.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediationCorner.Infrastructure.Data.Configurations
+{
+    internal class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
