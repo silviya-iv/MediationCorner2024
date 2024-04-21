88e3afc97191b3da5ecf8e3ebbbfbec7e230e908
ivanova.silviya@gmail.com <ivanova.silviya@gmail.com>
2024-04-21 23:58:41 +0300

    Admin area added and base and home controller

---
+++ MediationCorner/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediationCorner.Controllers
+{
+    
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
--- MediationCorner/Controllers/HomeController.cs
+++ MediationCorner/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using MediationCorner.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace MediationCorner.Controllers
 {
-    public class HomeController : Controller
+    [AllowAnonymous]
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -23,6 +25,25 @@ namespace MediationCorner.Controllers
             return View();
         }
 
+        public IActionResult ContactUs() 
+        {
+            return View();
+        }
+
+        public IActionResult AboutMediation()
+        {
+            return View();
+        }
+
+        public IActionResult LegalServices()
+        {
+            return View();
+        }
+
+        public IActionResult UsefulLinks() 
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
